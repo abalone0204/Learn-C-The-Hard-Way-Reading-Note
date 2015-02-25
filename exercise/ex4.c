9d16a7fecb8160bd8471e768be39ee47ab31c028
abalone0204 <abalone0204@gmail.com>
2015-02-26 02:38:55 +0800

    done ex4

---
+++ exercise/ex4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int age = 10;
+    int height = 100;
+    printf("I'am a %d old children\n", age);
+    printf("I'm %d inches tall\n", height);
+    return 0;
+}
