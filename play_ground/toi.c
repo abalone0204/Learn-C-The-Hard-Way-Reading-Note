dd3e706e91528b7f3425eee9f8dd938b7910bc98
abalone0204 <abalone0204@gmail.com>
2015-02-27 13:28:16 +0800

    ex17: done program and note; add a new dir to play with some example program which I'd like to test with

---
+++ play_ground/toi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    char *text = "20";
+    int text_to_i = atoi(text);
+    printf("Text: %s\n", text);
+    printf("Text to integer: %d\n", text_to_i);
+    return 0;
+}
