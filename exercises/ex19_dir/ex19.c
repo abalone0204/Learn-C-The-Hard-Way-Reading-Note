614e629ababca6f5e3d9f201e8ae9aa543d8354c
abalone0204 <abalone0204@gmail.com>
2015-02-28 22:34:56 +0800

    ex19: done code

--- exercises/ex19_dir/ex19.c
+++ exercises/ex19_dir/ex19.c
@@ -121,10 +121,52 @@ int process_input(Map *game)
 {
     printf("\n> ");
 
-    
+    char ch = getchar();
+
+    int damage = rand() % 4;
+
+    switch(ch) {
+        case -1:
+            printf("Giving up? You suck.\n");
+            return 0;
+            break;
+        case 'n':
+            game->_(move)(game, NORTH);
+            break;
+        case 'e':
+            game->_(move)(game, EAST);
+            break;
+        case 'w':
+            game->_(move)(game, WEST);
+            break;
+        case 's':
+            game->_(move)(game, SOUTH);
+            break;
+        case 'a':
+            game->_(attack)(game, damage);
+            break;
+        case 'l':
+            printf("You can go:\n");;
+            if(game->location->north) printf("NORTH\n");
+            if(game->location->east) printf("EAST\n");
+            if(game->location->west) printf("WEST\n");
+            if(game->location->south) printf("SOUTH\n");
+            break;
+        default:
+            printf("What?: %d\n", ch);
+    }
+    return 1;
+
 }
 
 int main(int argc, char *argv[]){
-    
+    // Simple way to setup the randomness
+    srand(time(NULL));
+
+    // make our map to work with
+    Map *game = NEW(Map, "The Hall of the Minotaur.");
+
+    printf("You enter the \n");
+    game->location->_(describe)(game->location);
     return 0;
 }
