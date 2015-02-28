b81f63718d044998cc68902f7c0a02eeb10c7da8
abalone0204 <abalone0204@gmail.com>
2015-02-28 11:03:37 +0800

    ex19: add ex19.h

---
+++ exercises/ex19_dir/ex19.h
@@ -0,0 +1,34 @@
+#ifndef _ex19_h
+#define _ex10_h
+
+#include "object.h"
+
+struct Monster {
+    Object proto;
+    int hit_points;
+};
+
+typedef struct Monster Monster;
+
+int Monster_attack(void *self, int damage);
+int Monster_init(void *self);
+
+
+struct Room {
+    Object proto;
+
+    Monster *bad_guy;
+
+    struct Room *north;
+    struct Room *east;
+    struct Room *west;
+    struct Room *south;
+};
+
+typedef struct Map Map;
+
+void *Map_move(void *self, Direction direction);
+int Map_attack(void *self, int damage);
+int Map_init(void *self);
+
+#endif
