0f3cc5f6ffdd6dcde78ff57e6a60dbec495f4532
abalone0204 <abalone0204@gmail.com>
2015-02-28 16:56:19 +0800

    ex19: check and move to process input

--- exercises/ex19_dir/ex19.c
+++ exercises/ex19_dir/ex19.c
@@ -29,6 +29,100 @@ int Monster_init(void *self)
     return 1;
 }
 
+// Simillar to the way using struct, check object.h
+
+Object MonsterProto = {
+    .init = Monster_init,
+    .attack = Monster_attack;
+};
+
+void *Room_move(void *self, Direction direction)
+{
+    Room *room = self;
+    Room *next = NULL;
+
+    if(direction == NORTH && room->north) {
+        printf("You go north, into: \n");
+        next = room->north;
+    } else if(direction == EAST && room->east) {
+        printf("You go east, into: \n");
+        next = room->east;
+    } else if(direction == WEST && room->west) {
+        printf("You go west, into: \n");
+        next = room->west;
+    } else if(direction == SOUTH && room->south){
+        printf("You go south, into: \n");
+        next = room->south;
+    } else {
+        printf("You can't go that direction.");
+        next = NULL;
+    }
+    return next;
+}
+
+int Room_attack(void *self, int damage)
+{
+    Room *room = self;
+    Monster *monster = room->bad_guy;
+
+    if(monster) {
+        monster->_(attack)(monster, damage);
+        return 1;
+    } else {
+        printf("You flail in the air at nothing. Idiot!\n", );
+        return 0;
+    }
+}
+
+Object RoomProto = {
+    .move = Room_move,
+    .attack = Room_attack;
+};
+
+void *Map_move(void *self, Direction direction)
+{
+    Map *map = self;
+    Room *location = map->location;
+    Room *next = NULL;
+
+    next = location->_(move)(location, direction);
+    if(next) {
+        map->location = next;
+    }
+
+    return next;
+}
+
+int Map_attack(void *self, int damage)
+{
+    Map *map = self;
+    Room *location = map->location;
+    // This line tell the player that they can't attack Map(or location wheter you prefer)
+    return location->_(attack)(location, damage);
+}
+
+int Map_init(void *self)
+{
+    Map *map = self;
+
+    // make some rooms for a small map
+    // Remember NEW, the macro we define in object.h?
+    Room *hall = NEW(Room, "The great Hall");
+    Room *throne = NEW(Room, "The throne room");
+    Room arena = NEW(Room, )
+}
+Object MapProto = {
+    .init = Map_init,
+    .attack = Map_attack,
+    .move = Map_move;
+};
+
+int process_input(Map *game)
+{
+    printf("\n> ");
+
+    
+}
 
 int main(int argc, char *argv[]){
     
